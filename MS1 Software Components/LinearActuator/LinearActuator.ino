262ae05c62f66d2615669c17e29c4738a15b49a3
Ryan Symons <47405201+rsymons22@users.noreply.github.com>
2026-02-19 18:08:23 -0500

    Added STL files and one minor cleanup to MS1 code

--- MS1 Software Components/LinearActuator/LinearActuator.ino
+++ MS1 Software Components/LinearActuator/LinearActuator.ino
@@ -1,18 +1,8 @@
-//Includes the Arduino Stepper Library
-#include <Stepper.h>
-
-// Defines the number of steps per rotation
-const int stepsPerRevolution = 2048;
-const int rpm = 10;
-
 const int extend_pin_in1 = 12;
 const int retract_pin_in2 = 13;
 
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-//Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
-
-void setup() {
+void setup()
+{
 
   Serial.begin(9600); // Start serial communication at 9600 baud rate
   Serial.println("Enter a character:");
@@ -23,40 +13,29 @@ void setup() {
   digitalWrite(retract_pin_in2, LOW);
 }
 
-void loop() {
+void loop()
+{
 
-  if (Serial.available() > 0) { // Check if data has been sent from the computer
+  if (Serial.available() > 0)
+  {                                    // Check if data has been sent from the computer
     char incomingByte = Serial.read(); // Read the oldest byte in the serial buffer
 
-    if(incomingByte == 'E') {
+    if (incomingByte == 'E')
+    {
       Serial.print("extend\n");
       digitalWrite(extend_pin_in1, HIGH);
       digitalWrite(retract_pin_in2, LOW);
     }
-    else if(incomingByte == 'R') {
+    else if (incomingByte == 'R')
+    {
       Serial.print("retract\n");
       digitalWrite(extend_pin_in1, LOW);
       digitalWrite(retract_pin_in2, HIGH);
-    } else if(incomingByte == 'S') {
+    }
+    else if (incomingByte == 'S')
+    {
       digitalWrite(extend_pin_in1, LOW);
       digitalWrite(retract_pin_in2, LOW);
     }
   }
-
-  // analogWrite(extend_pin_in1, 255);
-  // analogWrite(retract_pin_in2, 0);
-  // digitalWrite(extend_pin_in1, HIGH);
-  // digitalWrite(retract_pin_in2, LOW);
-
-  //digitalWrite(extend_pin_in1, HIGH);
-  //digitalWrite(retract_pin_in2, LOW);
-  
-  // myStepper.setSpeed(rpm);
-  // myStepper.step(stepsPerRevolution);
-  // delay(500);
-
-  // // Rotate CCW quickly at 10 RPM
-  // myStepper.setSpeed(rpm);
-  // myStepper.step(-stepsPerRevolution);
-  // delay(500);
 }
---
+++ STL Files/README.md
@@ -0,0 +1,23 @@
+# STL Files
+
+This folder contains all the STL files for the various 3D printed parts for the static and dynamic solar panel systems
+
+| File Name | Description | On Dynamic System? | On Static System? | Amount Used |
+|---|---|---|---|---|
+| ActuatorControllerMount.stl | Mount to affix the linear actuator controller to the turntable | ✓ | X | 1 |
+| CableHoleCap.stl | Cosmetic part to beautify the openings where wires go beneath the base | ✓ | X | 2 |
+| CableSwivelHolder.stl | Holder to allow the cable connecting the turntable and the base to swivel | ✓ | X | 1 |
+| DCLoadBracket.stl | Brackets to hold the DC loads to the base | ✓ | ✓ | 4 |
+| GyroMount.stl | Mount to affix the gyroscope to a panel's plywood backing | ✓ | X | 1 |
+| HalfBBMount.stl | Mount to affix a half breadboard down | ✓ | X | 1 |
+| LDRMount.stl | Mount to affix a <u>l</u>ight <u>d</u>ependent <u>r</u>esistor to a panel's plywood backing | ✓ | ✓ | 8 |
+| MotorWheelCap.stl | Cap to sit over the stepper motor's wheel, preventing it detaching | ✓ | X | 1 |
+| PanelClip.stl | Clip to hold a panel to it's backing | ✓ | ✓ | 6 |
+| PanelClipTH.stl | Clip to hold a panel to it's backing and have a temperature/humidity sensor attached | ✓ | ✓ | 2 |
+| PowerSupplyBracket.stl | Bracket to hold the power supply to the base | ✓ | X | 1 |
+| StaticCableSupport.stl | Support for the cable between the arduino and panel to rest on | X | ✓ | 1 |
+| StaticPanelHinge.stl | Hinge for a panel to articulate on if necessary | X | ✓ | 2 |
+| StaticPanelHolder.stl | Holder for a panel to rest on | X | ✓ | 2 |
+| StepperControllerMount.stl | Mount to affix the stepper motor controller to the base | ✓ | X | 1 |
+| StepperMotorMount.stl | Mount to affix the stepper motor to the base and up against the turntable | ✓ | X | 1 |
+| UnoHalfBBMount.stl | Mount to affix an Arduino UNO and a half breadboard down | ✓ | ✓ | 2 |
